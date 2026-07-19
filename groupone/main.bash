85534f118bdb41dcb003f809b9ba7071237645e7
Demcalves <adahik56@gmail.com>
2026-07-19 00:00:15 -0400

    y

--- groupone/main.bash
+++ groupone/main.bash
@@ -2,6 +2,20 @@
 set -euo pipefail
 
 # run the conda_env_setup
-bash conda_env_setup.bash
+#bash conda_env_setup.bash
+
+
+BASE_DIR=$(pwd)
+PROJ_DIR="${BASE_DIR}/groupone"
+RAW_DATA="${PROJ_DIR}/rawdata/raw"
+SRA_LIST="${PROJ_DIR}/sra.txt"
 
 # get raw data using xargs
+cat sraToAdd.txt
+while read LINE; do
+    if [ ! -d "${RAW_DATA}/$LINE" ]; then
+        echo "${LINE} is not downloaded, adding to textfile for download"
+        echo $LINE >> sraToAdd.txt
+    else
+        echo "${LINE} is already downloaded in ${RAW_DATA}"
+done < sra.txt
