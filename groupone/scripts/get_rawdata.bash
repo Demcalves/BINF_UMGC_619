00f38072edefab00ea3b1d4594108eb37da7cf16
Demcalves <adahik56@gmail.com>
2026-07-19 20:27:29 -0400

    completed main script up to adding data

--- groupone/scripts/get_rawdata.bash
+++ groupone/scripts/get_rawdata.bash
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 
 set -eou pipefail
-path=$(pwd)
-echo "${path}"
+BASE_DIR=$(pwd)
+echo "${BASE_DIR}"
 
 SRA=$1
 # set directory
 
-PROJ_DIR="${path}/groupone"
-RAW_DATA="${PROJ_DIR}/00_rawdata/raw"
-
+PROJ_DIR="${BASE_DIR}/groupone"
+RAW_DATA="${PROJ_DIR}/rawdata/raw"
+SRX_LIST="${PROJ_DIR}/sraToAdd.txt"
+# get length of entries in SRX_LIST
+SRX_DEPTH=$(grep -v "\s*$" $SRX_LIST | wc -l)
+# calculate number of available threads for concurrent download
+MAX_THREAD=8
+THREAD_COUNT=$(($MAX_THREAD / $SRX_DEPTH))
 # gather raw_data read
-if [ ! -d "${SRA}" ]; then
-    prefetch ${SRA}
-    fasterq-dump ${SRA} -O ${RAW_DATA} --split-files --threads 2 # this download might be slow
-fi
+prefetch $SRA -O $RAW_DATA
+fasterq-dump $SRA -O $RAW_DATA --split-files --threads $THREAD_COUNT # this download might be slow
