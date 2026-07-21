d08efa99183843ddefda5a2591d6372739b39c17
Andrew Dahik <117870290+Demcalves@users.noreply.github.com>
2026-07-20 22:02:56 -0400

    Update main.bash
    add fastqc step

--- groupone/main.bash
+++ groupone/main.bash
@@ -1,37 +1,50 @@
-#!/usr/bin/env bash
 set -euo pipefail
 
 # run the conda_env_setup
 #bash conda_env_setup.bash
 
 
-BASE_DIR=$(pwd)
-PROJ_DIR="${BASE_DIR}/groupone"
+PROJ_DIR=$(pwd)
+#PROJ_DIR="${BASE_DIR}/groupone"
 RAW_DATA="${PROJ_DIR}/rawdata/raw"
-SRA_LIST="${PROJ_DIR}/sra.tsv"
+SRA_LIST="${PROJ_DIR}/sra.txt"
 # get raw data using xargs
 touch ${PROJ_DIR}/sraToAdd.txt
-touch "${PROJ_DIR}/srr.txt"
-awk '{print $1}' $SRA_LIST >> ${PROJ_DIR}/srr.txt
-SRR_LIST="${PROJ_DIR}/srr.txt"
-SRX_LIST="${PROJ_DIR}/sraToAdd.txt"
+touch ${PROJ_DIR}/srr.txt
+
+DWND_LIST="${PROJ_DIR}/sraToAdd.txt" # Download list, temporary file
 
 while read LINE; do
     if [ ! -d "${RAW_DATA}/${LINE}" ]; then
         echo "${LINE} is not downloaded, adding to textfile for download"
-        awk -v line="$LINE" '$1 == line {print $1}' "$SRA_LIST" >> $SRX_LIST
+        cat $LINE >> $DWND_LIST
     else
         echo "${LINE} is already downloaded in ${RAW_DATA}"
     fi
-done < $SRR_LIST
-# clean up
-# $SRX_LIST
-rm $SRR_LIST
+done < $SRA_LIST
+
 
 # calculate number of concurrent operations needed
-# add data now with $SRX_LIST using xargs
+# add data now with $DWND_LIST using xargs
 # enable excution privelges
-RAW_DATA_SCRIPT="${PROJ_DIR}/scripts/get_rawdata.bash"
-chmod -x "${RAW_DATA_SCRIPT}"
-xargs -a "${SRX_LIST}" -P 4 -I{} bash "$RAW_DATA_SCRIPT" {}
-rm $SRX_LIST 
+if [ $(grep -v "\s+$" $DWND_LIST) > 0 ]; then
+
+    RAW_DATA_SCRIPT="${PROJ_DIR}/scripts/get_rawdata.bash"
+    chmod -x "${RAW_DATA_SCRIPT}"
+    # this runs the get raw data script
+    xargs -a "${DWND_LIST}" -P 4 -I{} bash "$RAW_DATA_SCRIPT" {}
+fi
+# clean up step
+rm $DWND_LIST 
+
+# Run FastQC on Gathered files in rawdata/raw
+# Run FastQC on both files
+while read LINE; do
+    if [ ! -f "$PROJ_DIR/results/qc/${LINE}_1_fastqc.html" ]; then
+        echo "Performing fastqc..."
+        fastqc "$RAW_DATA/${LINE}_1.fastq" "$RAW_DATA/${LINE}_2.fastq" \
+            -o "$PROJ_DIR/results/qc" -t 2
+        echo "FastQC complete! Reports:"
+        ls "$PROJ_DIR/results/qc"/*.html
+    fi
+done < $SRA_LIST
