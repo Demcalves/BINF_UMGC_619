9e9e01935023a6d418fd32cc15cd6eeb9f1c43b7
Andrew Dahik <117870290+Demcalves@users.noreply.github.com>
2026-07-21 21:27:05 -0400

    Update make_dir.bash
    Removed "scripts" from mkdir call as the repository has the scripts subdirectory already.

--- groupone/scripts/make_dir.bash
+++ groupone/scripts/make_dir.bash
@@ -5,5 +5,5 @@ echo "${path}"
 # set directory
 # Create a project directory
 if [ ! -d "groupone" ]; then
-    mkdir -p groupone/{rawdata/{raw,reference},results/{qc,trimmed,aligned,counts,de,figures},scripts}
+    mkdir -p groupone/{rawdata/{raw,reference},results/{qc,trimmed,aligned,counts,de,figures}}
 fi
