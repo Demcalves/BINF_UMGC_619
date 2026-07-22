8f5e08a12fc38d39f9ca913776d7c215ef094464
Andrew Dahik <117870290+Demcalves@users.noreply.github.com>
2026-07-21 21:00:47 -0400

    Update main.bash
    Updated main script to include fastp workflow block

--- groupone/main.bash
+++ groupone/main.bash
@@ -7,7 +7,7 @@ set -euo pipefail
 
 PROJ_DIR=$(pwd)
 #PROJ_DIR="${BASE_DIR}/groupone"
-RAW_DATA="${PROJ_DIR}/rawdata/raw"
+RAW_DATA="${PROJ_DIR}/data/raw"
 SRA_LIST="${PROJ_DIR}/sra.txt"
 # get raw data using xargs
 touch ${PROJ_DIR}/sraToAdd.txt
@@ -38,32 +38,45 @@ fi
 # clean up step
 rm $DWND_LIST 
 
-# Get list of SRR files that need to be ran through FASTQC
-touch ${PROJ_DIR}/fastqc_list.txt
-FASTQC_LIST="${PROJ_DIR}/fastqc_list.txt"
+# For the rest of the workflow, it is naively assumed that if the FASTQC step below 
+# for an expected result has not been performed, that the subsequent steps of trimming, 
+# aligning and differential expression have also not been performed. The WORKFLOW_LIST
+# will now hold all lines of SRR identifiers to manipulate the raw data pulled above.
+# nevertheless, each step in this RNA-Seq workflow will be isolated from other steps
+# so that they are at least checked to have been performed.
+
+touch ${PROJ_DIR}/workflow_list.txt
+WORKFLOW_LIST="${PROJ_DIR}/workflow_list.txt"
 
 # Run FastQC on Gathered files in rawdata/raw
 # Run FastQC on both files
+
 while read LINE; do
     echo $LINE
     if [ ! -f "$PROJ_DIR/results/qc/${LINE}_1_fastqc.html" ]; then
         
-        echo "FASTQC for ${LINE} is not complete! Adding to ${FASTQC_LIST} for FASTQC"
-        cat $LINE >> $FASTQC_LIST
+        echo "FASTQC for ${LINE} is not complete! Adding to ${WORKFLOW_LIST} for FASTQC"
+        cat $LINE >> $WORKFLOW_LIST
         
     fi
 done < $SRA_LIST
 
-if [ $(grep -v "\s+$" $FASTQC_LIST) > 0 ]; then
+if [ $(grep -v "\s+$" $WORKFLOW_LIST) > 0 ]; then
+    
     echo "Performing fastqc..."
     FASTQC_SCRIPT="${PROJ_DIR}/scripts/01_run_fastqc.bash"
     chmod -x "${FASTQC_SCRIPT}"
     # this runs the get raw data script
-    xargs -a "${FASTQC_LIST}" -P 4 -I{} bash "$FASTQC_SCRIPT" {}
+    xargs -a "${WORKFLOW_LIST}" -P 4 -I{} bash "$FASTQC_SCRIPT" {}
+
+
+    echo "FastQC complete for all test articles! Reports:"
+    ls "$PROJ_DIR/results/qc"/*.html
+
+    # Moving on to read trims, using fastp
+    echo "Performing fastp trimming ..."
+    FASTP_SCRIPT="${PROJ_DIR}/scripts/02_run_fastp.bash"
+    chmod -x "${FASTP_SCRIPT}"
+    # this runs the get raw data script
+    xargs -a "${WORKFLOW_LIST}" -P 4 -I{} bash "$FASTP_SCRIPT" {}
 fi
-
-echo "FastQC complete for all test articles! Reports:"
-ls "$PROJ_DIR/results/qc"/*.html
-
-# clean up 
-rm $FASTQC_LIST
