e9fa7cbf4eb2da8ba697eff6558a5cd2d7eb0264
Andrew Dahik <117870290+Demcalves@users.noreply.github.com>
2026-07-21 20:59:44 -0400

    Update 01_run_fastqc.bash
    updated variables in fastqc to reflect semantic design

--- groupone/scripts/01_run_fastqc.bash
+++ groupone/scripts/01_run_fastqc.bash
@@ -7,12 +7,12 @@ SRA=$1
 # set directory
 
 RAW_DATA="${PROJ_DIR}/rawdata/raw"
-SRX_LIST="${PROJ_DIR}/sraToAdd.txt" # list of sra to run fastqc on, used for allocating resources for running multiple fastqc
-# get length of entries in SRX_LIST
-SRX_DEPTH=$(grep -v "\s*$" $SRX_LIST | wc -l)
+FASTQC_LIST="${PROJ_DIR}/workflow_list.txt" # list of sra to run fastqc on, used for allocating resources for running multiple fastqc
+# get length of entries in FASTQC_LIST
+FASTQC_DEPTH=$(grep -v "\s*$" $FASTQC_LIST | wc -l)
 # calculate number of available threads for concurrent download
 MAX_THREAD=8
-THREAD_COUNT=$(($MAX_THREAD / $SRX_DEPTH))
+THREAD_COUNT=$(($MAX_THREAD / $FASTQC_DEPTH))
 # gather raw_data read
 fastqc "$RAW_DATA/${LINE}_1.fastq" "$RAW_DATA/${LINE}_2.fastq" \
             -o "$PROJ_DIR/results/qc" -t $THREAD_COUNT
